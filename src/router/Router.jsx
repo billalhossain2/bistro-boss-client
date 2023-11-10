208bddf56acefa5c7b65d3fe83a6aceddde8201e
billal-webdev <billal-webdev@gmail.com>
2023-11-10 17:01:39 +0600

    setup basic folder structure and install required dependencies

---
+++ src/router/Router.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root";
+import Home from "../pages/Home/Home";
+import Shop from "../pages/Shop/Shop";
+import Menu from "../pages/Menu/Menu";
+import Contact from "../pages/Contact/Contact";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:"/shop",
+        element:<Shop></Shop>
+      },
+      {
+        path:"/menu",
+        element:<Menu></Menu>
+      },
+      {
+        path:"/contact",
+        element:<Contact></Contact>
+      },
+      {
+        path:"/login",
+        element:<Login></Login>
+      },
+      {
+        path:"/signup",
+        element:<Signup></Signup>
+      },
+    ]
+  },
+]);
+
+export default router;
